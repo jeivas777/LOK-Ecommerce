680c3d20c00fd4ff9fb94c0695778e0163f421b7
VitorOliveira <jvdeus27@gmail.com>
2025-01-06 17:38:42 -0300

    itens list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header.component';
+import { ContentItensComponent } from './content-itens/content-itens.component';
+import { ContentItemComponent } from './content-itens/content-item/content-item.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HeaderComponent, ContentItensComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
