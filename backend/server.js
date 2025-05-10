ee4434d75f28afecc637e28b76e4e1c59a4932c0
Vitor <jvdeus27@gmail.com>
2025-05-10 13:41:21 -0300

    commit

--- backend/server.js
+++ backend/server.js
@@ -1,40 +1,36 @@
-require('dotenv').config();
+const { Pool } = require("pg");
+require("dotenv").config();
 const express = require("express");
-const mysql = require("mysql2/promise");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const productRoutes = require("./routes/productRoutes");
 
 const app = express();
 
-const dbConfig = {
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
+const pool = new Pool({
+  host: process.env.DB_HOST, // Ex: dpg-xxxxxx
+  user: process.env.DB_USER, // Ex: lok_postgresql_user
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: parseInt(process.env.DB_PORT),
-};
-
-let connection;
+  database: process.env.DB_NAME, // Ex: lok_postgresql
+  port: parseInt(process.env.DB_PORT || "5432"),
+  ssl: true, // Render exige SSL para conexões externas
+});
 
 app.use(async (req, res, next) => {
   try {
-    if (!connection) {
-      connection = await mysql.createConnection(dbConfig);
-      console.log("MySQL conectado com sucesso :)");
-    }
-
-    req.db = connection;
+    req.db = pool;
     next();
   } catch (err) {
-    console.error("Erro ao conectar ao MySQL", err);
-    res.status(500).json({ error: "Erro ao conectar ao MySQL" });
+    console.error("Erro ao conectar ao PostgreSQL", err);
+    res.status(500).json({ error: "Erro ao conectar ao PostgreSQL" });
   }
 });
 
-app.use(cors({
-  origin: ['http://localhost:4200', 'https://seu-front-no-vercel.vercel.app'], // ou '*'
-}));
+app.use(
+  cors({
+    origin: ["http://localhost:4200", "https://seu-front-no-vercel.vercel.app"],
+  })
+);
 app.use(bodyParser.json());
 app.use("/api", productRoutes);
 
