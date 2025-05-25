dc45ead581bae349195983f42e80e09eb89f5857
Vitor <jvdeus27@gmail.com>
2025-05-25 18:11:54 -0300

    commit

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const authRoutes = require("./routes/authRoutes");
 const productRoutes = require("./routes/productRoutes");
 
 const app = express();
