21bf2f486a5e965d0842080c23afb0aac8eb6e87
Vitor <jvdeus27@gmail.com>
2025-05-28 08:45:07 -0300

    commiit

--- frontend/src/app/ecommerce/services/product.service.ts
+++ frontend/src/app/ecommerce/services/product.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, Observable } from 'rxjs';
-import { environment } from '../../../enviroments/enviroments';
+import { environment } from '../../../environments/environment';
 
 export interface ProductResponse {
   products: Product[];
